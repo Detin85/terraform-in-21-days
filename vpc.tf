74ddc3743d156b0a440237ed604bce595efd5b2c
Detin Jani <75448813+Detin85@users.noreply.github.com>
2025-04-07 16:56:45 +0200

    Add ec2 instance (#16)
    Add ec2 instance

--- vpc.tf
+++ vpc.tf
@@ -11,11 +11,11 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
- 
- count = length(var.public_cidr)
- 
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_cidr[count.index]
+
+  count = length(var.public_cidr)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
@@ -28,9 +28,9 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
 
   count = length(var.private_cidr)
-  
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_cidr[count.index]
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
@@ -61,20 +61,20 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
- 
- count = length(var.public_cidr)
- 
+
+  count = length(var.public_cidr)
+
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_eip" "nat" {
-  
+
   count = length(var.public_cidr)
-  
+
   domain = "vpc"
-  
-  
+
+
   tags = {
     Name = "${var.env_code}-nat-main ${count.index + 1}"
 
@@ -82,9 +82,9 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_nat_gateway" "nat-main" {
- 
- count = length(var.public_cidr)
- 
+
+  count = length(var.public_cidr)
+
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
@@ -100,13 +100,13 @@ resource "aws_nat_gateway" "nat-main" {
 
 
 resource "aws_route_table" "private" {
-  
+
   count = length(var.private_cidr)
-  
+
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat-main[count.index].id
   }
 
@@ -118,9 +118,9 @@ resource "aws_route_table" "private" {
 
 
 resource "aws_route_table_association" "private" {
-  
+
   count = length(var.private_cidr)
-  
+
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
