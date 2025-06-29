ffd38a84edb96454294a2bfcdd27835aea5f02cd
Detin Jani <detin_jani@hotmail.com>
2025-06-29 13:04:42 +0200

    changes

--- instance.tf
+++ instance.tf
@@ -1,101 +1,101 @@
-data "aws_ami" "amazonlinux" {
-  most_recent = true
+#data "aws_ami" "amazonlinux" {
+#  most_recent = true
+#
+#
+#   name   = "name"
+#    values = ["amzn2-ami-kernel-*"]
+#  }
+#
+#  filter {
+#    name   = "virtualization-type"
+#    values = ["hvm"]
+#  }
+#}
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-*"]
-  }
+#resource "aws_instance" "public" {
+#  ami                         = data.aws_ami.amazonlinux.id
+#  associate_public_ip_address = true
+#  instance_type               = "t3.nano"
+#  key_name                    = "main"
+#  vpc_security_group_ids      = [aws_security_group.public.id]
+#  subnet_id                   = aws_subnet.public[0].id
+#  user_data                   = file("/home/djani/Documents/Detin/Terraform_Tutorials/terraform-in-21-days/user_data.sh")
+#
+#  tags = {
+#    Name = "$(var.env_code)-public"
+#  }
+#}
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+#resource "aws_security_group" "public" {
+#  name        = "$(var.env_code)-public"
+#  description = "Allow inbound traffic"
+#  vpc_id      = aws_vpc.main.id
 
-resource "aws_instance" "public" {
-  ami                         = data.aws_ami.amazonlinux.id
-  associate_public_ip_address = true
-  instance_type               = "t3.nano"
-  key_name                    = "main"
-  vpc_security_group_ids      = [aws_security_group.public.id]
-  subnet_id                   = aws_subnet.public[0].id
-  user_data                   = file("/home/djani/Documents/Detin/Terraform_Tutorials/terraform-in-21-days/user_data.sh")
-
-  tags = {
-    Name = "$(var.env_code)-public"
-  }
-}
-
-resource "aws_security_group" "public" {
-  name        = "$(var.env_code)-public"
-  description = "Allow inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "SSH from public"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["81.26.206.228/32"]
-  }
-
-
-  ingress {
-    description = "HTTP from public"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    description = "SSH from public"
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["81.26.206.228/32"]
+#  }
 
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    description = "HTTP from public"
+#    from_port   = 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-  tags = {
-    Name = "$(var.env_code)-public"
-  }
-}
 
-resource "aws_instance" "private" {
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-  ami                         = data.aws_ami.amazonlinux.id
-  associate_public_ip_address = true
-  instance_type               = "t3.nano"
-  key_name                    = "main"
-  vpc_security_group_ids      = [aws_security_group.private.id]
-  subnet_id                   = aws_subnet.private[0].id
+#  tags = {
+#    Name = "$(var.env_code)-public"
+#  }
+#}
 
-  tags = {
-    Name = "$(var.env_code)-private"
-  }
-}
+#resource "aws_instance" "private" {
 
-resource "aws_security_group" "private" {
-  name        = "$(var.env_code)-private"
-  description = "Allow VPC traffic"
-  vpc_id      = aws_vpc.main.id
+#  ami                         = data.aws_ami.amazonlinux.id
+#  associate_public_ip_address = true
+#  instance_type               = "t3.nano"
+#  key_name                    = "main"
+#  vpc_security_group_ids      = [aws_security_group.private.id]
+#  subnet_id                   = aws_subnet.private[0].id
 
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
-  }
+#  tags = {
+#    Name = "$(var.env_code)-private"
+#  }
+#}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#resource "aws_security_group" "private" {
+#  name        = "$(var.env_code)-private"
+#  description = "Allow VPC traffic"
+#  vpc_id      = aws_vpc.main.id
 
-  tags = {
-    Name = "$(var.env_code)-private"
-  }
-}
+#  ingress {
+#    description = "SSH from VPC"
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = [var.vpc_cidr]
+#  }
+
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+
+#  tags = {
+#    Name = "$(var.env_code)-private"
+#  }
+#}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "env_code" {}
+
+variable "vpc_cidr" {}
+
+variable "public_subnet_cidr" {}
+
+variable "private_subnet_cidr" {}
+
+variable "availability_zones" {}
