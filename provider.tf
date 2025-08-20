7c5c12ccd264a64971adc31505beb5c2f99d86f1
Detin Jani <detin_jani@hotmail.com>
2025-08-20 15:02:59 +0200

    Changes

--- provider.tf
+++ provider.tf
@@ -2,10 +2,11 @@ terraform {
 
   backend "s3" {
 
-    bucket = "detin-demo-v1"
-    key = "terraform.tfstate"
-    region = "eu-central-1"
+    bucket         = "detin-demo-v1"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
     dynamodb_table = "terraform-remote-state"
+    encrypt        = true
   }
 }
 
