63fab0552525175df762d5470d16605f76f6e752
Detin Jani <detin_jani@hotmail.com>
2025-08-21 13:32:32 +0200

    CHanges

--- level2/data.tf
+++ level2/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "level1" {
   backend = "s3"
   
   config = {
-    bucket         = "terraform-remote-state"
+    bucket         = "detin-demo-v1"
     key            = "level1.tfstate"
     region         = "eu-central-1"
   }
